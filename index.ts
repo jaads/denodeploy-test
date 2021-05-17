d094e88d22577510ddfb39d82e1c9bb34faa7ebe
Jan Arends <jan.arends@mailbox.org>
2021-05-18 00:06:05 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+interface answer {
+  hello: string;
+  world: string;
+}
+
+function handleRequest(params) {
+  const json = JSON.stringify({
+    message: "Hello from Deno Deploy",
+  });
+
+  return new Response(json, {
+    headers: {
+      "content-type": "application/json; charset=UTF-8",
+    },
+  });
+}
+
+function getAnswer(param: number): answer {
+  const anAnswer: answer = {
+    hello: "hello",
+    world: "world",
+  };
+  return anAnswer;
+}
+
+function createResponse(): answer[] {
+  const answers: answer[] = [];
+  answers.push(getAnswer(1));
+  answers.push(getAnswer(2));
+  answers.push({ lorem: "ipsum2" });
+  return answers;
+}
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
